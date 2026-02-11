65294e47a82ac6b912151938661f88bf4d171dfd
Vivek <vivek250391@gmail.com>
2026-02-11 21:03:59 +0530

    signup functionality

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,7 +1,36 @@
+'use client'
+import Styles from '@/components/auth/Signup.module.css'
+import {setUsername,setConfirmPassword,setPassword,setEmail,Validate} from '@/lib/auth/signupeventhandler'
+import { useState } from 'react';
 export function SignUp(){
+    const [signupForm,setSignupForm]=useState({username:'',password:'',confirmPassword:'',email:''}) 
+    const [error,setError]=useState('')
+    const [success,setSuccees]=useState('')
     return (
-        <div>
-            signup module
+       <div className={Styles.Login}>
+            <div className={Styles.Header}>Login</div>
+            <div>
+                <input type="text" size="40" placeholder="username" onChange={(event)=>setUsername(event,signupForm,setSignupForm)} />
+            </div>
+            <div>
+                <input type="password" size="40" placeholder="password" onChange={(event)=>setPassword(event,signupForm,setSignupForm)} />
+            </div>
+            <div>
+                <input type="password" size="40" placeholder="confirm password" onChange={(event)=>setConfirmPassword(event,signupForm,setSignupForm)} />
+            </div>
+            <div>
+                <input type="text" size="40" placeholder="email" onChange={(event)=>setEmail(event,signupForm,setSignupForm)} />
+            </div>
+            <div className={Styles.error}>
+                {error}
+            </div>
+            <div className={Styles.success}>
+                {success}
+            </div>
+            <div>
+                <button type="button" onClick={()=>Validate(signupForm,setError,setSuccees)}>sign Up</button>
+            </div>
+            
         </div>
     );
 }
---
+++ frontend/src/lib/auth/signupeventhandler.js
@@ -0,0 +1,58 @@
+export function setUsername(event,signupForm,setSignupForm){
+    setSignupForm({...signupForm,username:event.target.value})
+}
+
+export function setPassword(event,signupForm,setSignupForm){
+    setSignupForm({...signupForm,password:event.target.value})
+}
+
+export function setConfirmPassword(event,signupForm,setSignupForm){
+    setSignupForm({...signupForm,confirmPassword:event.target.value})
+}
+
+export function setEmail(event,signupForm,setSignupForm){
+    setSignupForm({...signupForm,email:event.target.value})
+}
+
+export async function Validate(signupForm,setError,setSuccess){
+    setError('')
+    setSuccess('')
+    if (signupForm.username===''){
+            setError('login is empty')
+            return;
+    }
+    if (signupForm.email===''){
+            setError('email is empty')
+            return;
+    }
+    if (signupForm.password===''){
+            setError('password is empty')
+            return;
+    }
+    if (signupForm.confirmPassword===''){
+            setError('confirm password is empty')
+            return;
+    }
+    if(signupForm.password!==signupForm.confirmPassword){
+        setError('confirm password and password is not same')
+        return;
+    }
+    addlogin(signupForm,setError,setSuccess)
+}
+
+async function addlogin(signupForm,setError,setSuccess){
+    let data={}
+    console.log(process.env.NEXT_PUBLIC_API_URL)
+    const response =await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify({username:signupForm.username,password:signupForm.password,email:signupForm.email})
+    })
+
+    if (response.status===200)
+        setSuccess("signup done succefully")
+    else
+        setError(response.status)
+}
