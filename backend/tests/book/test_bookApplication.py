3fa3bd19eed28355b91cdf99ae815de7b0b8fa79
Vivek <vivek250391@gmail.com>
2026-02-14 21:32:46 +0530

    unit testing

---
+++ backend/tests/book/test_bookApplication.py
@@ -0,0 +1,41 @@
+from mock import patch,Mock
+import pytest
+from uuid import uuid4
+from book.bookapplication import bookApplication
+from book.bookModel import BookModel
+from book.bookRepository import bookRepository
+from book.bookEntity import book
+
+
+
+
+def test_book_application_Positive():
+    bookentities=[book(id=uuid4(),title="fake title",path="fake path",author="fake author"),]
+    bookRepository.get=Mock(return_value=bookentities)
+    x=BookModel(id=uuid4())
+    a=bookApplication()  
+    data=a.get()
+    assert len(data)==1
+
+def test_book_application_add_positive():
+    bookRepository.add=Mock(return_value=None)
+    model=BookModel(id=uuid4(),title="fake title",path="fake path",author="fake author")
+    a=bookApplication()
+    data=a.add(model)
+    Mock.assert_called_once(bookRepository.add)
+    assert data==None
+
+def test_book_application_update_positive():
+    bookRepository.update=Mock(return_value=None)
+    model=BookModel(id=uuid4(),title="fake title",path="fake path",author="fake author")
+    a=bookApplication()
+    data=a.update(uuid4(),model)
+    Mock.assert_called_once(bookRepository.update)
+    assert data==None
+
+def test_book_application_delete_positive():
+    bookRepository.delete=Mock(return_value=None)
+    a=bookApplication()
+    data=a.delete(id=uuid4())
+    Mock.assert_called_once(bookRepository.delete)
+    assert data==None
