5d700918162c0a31ea445f5564920f6cc8493dfa
Vivek <vivek250391@gmail.com>
2026-02-28 18:57:28 +0530

    implemented vector search

--- backend/aiagent/init.py
+++ backend/aiagent/init.py
@@ -1,5 +1,9 @@
 import ollama
 import os
+import faiss
+from nomic import embed
+import numpy as np
+import asyncio
 from uuid import UUID
 from dotenv import load_dotenv
 from book.bookissue.bookissueapplication import BookIssueApplication
@@ -9,7 +13,38 @@ from aiagent.aiapplication import AiApplication
 bi=BookIssueApplication()
 b=bookApplication()
 
+def vector_index_generation():
+    d = 64 # Dimensionality of vectors
+    n_vectors = 10000
+    
+    books=b.get()
+    data=[]
+    if(len(books)==0):
+        return
+    for book in books:
+        print(book.id)
+        data.append(f"{book.id} {book.author} {book.genre}")
+    output=embed.text(
+        texts=data,
+        model="nomic-embed-text-v1.5",
+        inference_mode="local")
+    #print(f"####{output['embeddings']}")
+    embeddings=np.array(output['embeddings']).astype('float32')
+    dimension=embeddings.shape[1]
+    index=faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+    print(f"total vectors in index:{index.ntotal}")
+    faiss.write_index(index,"nomic_faiss_index.bin")
+    #loaded_index = faiss.read_index("nomic_faiss_index.bin")
+
+
+
+
+
+    return 1
+
 async def greet_message():
+    vector_index_generation()
     await ai_summary()
     print("hello world 2026")
     return "hello world 2026"
--- backend/aiagent/router.py
+++ backend/aiagent/router.py
@@ -1,7 +1,12 @@
+from nomic import embed
+import faiss
+import numpy as np
 from fastapi import APIRouter
 from aiagent.aiapplication import AiApplication
+from book.bookapplication import bookApplication
 a=AiApplication()
 ai_router=APIRouter()
+b=bookApplication()
 
 @ai_router.get("/api/books/{id}/analysis")
 def get_review(id):
@@ -10,4 +15,33 @@ def get_review(id):
 
 @ai_router.get("/api/recommendations")
 def get_recommendations():
-    pass
+    books=b.get()
+    query_text = "title22"
+
+    output = embed.text(
+            texts=[query_text],
+            model='nomic-embed-text-v1.5', # Or other Nomic model
+        task_type='search_query',
+        inference_mode="local"
+    )
+
+    # The resulting vector is in output['embeddings'][0]
+    query_vector = output['embeddings']
+    print(query_vector)
+    index = faiss.read_index("nomic_faiss_index.bin")
+    k = 2
+    query_vector_np=np.array(query_vector)
+    # 3. Perform the search
+    # D = distances, I = indices of nearest neighbors
+    d,i = index.search(query_vector_np, k)
+
+    print("Nearest neighbor indices:", d)
+    print("Distances:", i)
+    book_data=[]
+    for i, index_id in enumerate(i[0]):
+    # Use the retrieved index to access the original sentence from the list
+        original_text = books[index_id]
+        book_data.append(original_text.title)
+        distance = d[0][i]
+        print(f"* Neighbor {i+1}: {original_text} (Distance: {distance:.4f})")
+    return book_data
